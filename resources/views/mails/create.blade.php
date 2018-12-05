3811e7ed28e28b6fb221d5c3736b84c7011ceb02
Nabihah <nabihah.student@gmail.com>
2018-12-05 15:57:27 +0800

    create blade

--- resources/views/mails/create.blade.php
+++ resources/views/mails/create.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     <h1>Create New Email</h1>
-    {!! Form::open(['action' => 'PostsController@store','method' =>'POST']) !!}
+    {!! Form::open(['action' => 'MailsController@store','method' =>'POST']) !!}
         <div class="form-group">
             {{Form::label('title','Title')}}
             {{Form::text('title','',['class'=>'form-control','placeholder' => 'Title'])}}

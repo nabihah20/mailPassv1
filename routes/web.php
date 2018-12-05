b2d945aab1bce31e5487f016b810a5b6577b4f28
Nabihah <nabihah.student@gmail.com>
2018-12-05 12:44:01 +0800

    tambag resource

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider');
 Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
 
-Route::resource('mails', 'MailsController@index');
+Route::resource('/mails','MailsController');

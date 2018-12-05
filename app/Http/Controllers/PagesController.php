963c2db2438efb7ef91baf99a840e485416f203a
Nabihah <nabihah.student@gmail.com>
2018-12-05 09:46:23 +0800

    add pages controller

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        //return 'INDEX';
+        return view ('pages.index');
+    }
+
+    public function about(){
+        return view ('pages.about');
+    }
+}

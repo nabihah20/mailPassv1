2df77e1b8e703713432aea2394e5de7e4f50371e
Nabihah <nabihah.student@gmail.com>
2018-12-07 14:49:50 +0800

    add delete button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,25 +7,25 @@
 @section('content')
 <div class="jumbotron">
     <h2>Dashboard</h2>
-    <a class="btn btn-primary btn-lg" href="/composemail" role="button"><span class="glyphicon glyphicon-plus"></span>  Compose </a>
+    <a class="btn btn-info btn-lg" href="/composemail" role="button"><span class="glyphicon glyphicon-plus"></span>  Compose </a>
     <br/><br/>
             @if (count($mails) > 0)
             <table class="table table-striped">
                 <tr>
                     <th>Subject</th>
-                    <th></th>
                     <th>Action</th>
+                    <th>Status</th>
                 </tr>
                     @foreach($mails as $mail)
                         <tr>
                             <td><a href="/mails/{{$mail->id}}/">{{$mail->subject}}</a></td>
-                            <td></td>
                             <td>
-                                {!!Form::open(['action'=>['MailsController@destroy',$mail->id],'method'=>'POST','class'=>'pull-left'])!!}
+                                {!!Form::open(['action'=>['MailsController@destroy',$mail->id],'method'=>'POST','class'=>'pull-center'])!!}
                                     {{Form::hidden('_method','DELETE')}}
                                     {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
                                 {!! Form::close() !!}  
                             </td>
+                            <td></td>
                         </tr>
                     @endforeach
             </table>
